385f4df5b6a9f4b27539c607f2f28bdcb0b27a18
omarfaruk7022 <farhanfaruk221@gmail.com>
2025-04-05 22:14:22 +0600

    Chat updated with gemini 1.5 model

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -24,7 +24,6 @@
 // }
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export default async function handler(req, res) {
@@ -40,7 +39,8 @@ export default async function handler(req, res) {
     }
 
     const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-flash",
+      //   model: "gemini-1.5-flash",
+      model: "gemini-1.5-pro",
     });
     const result = await model.generateContent(message);
     const response = await result.response;
